771f022e058c60f298a5c06ca7459baef0e34d96
Mohammad Banisaeid <smbl64@gmail.com>
2022-08-26 21:57:06 +0200

    Add skeleton files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
-pub fn run_file(filename: String) {}
+use std::io::Write;
 
-pub fn run_prompt() {}
+pub fn run_file(filename: &str) -> Result<(), anyhow::Error> {
+    let content = std::fs::read_to_string(filename)?;
+    run(content.as_ref())
+}
+
+pub fn run_prompt() -> Result<(), anyhow::Error> {
+    let reader = std::io::stdin();
+
+    loop {
+        print!("> ");
+        std::io::stdout().flush().expect("failed to flush stdout");
+
+        let mut line = String::new();
+        let n = reader.read_line(&mut line)?;
+        if n == 0 {
+            return Ok(());
+        }
+        let line = line.trim_end();
+        run(line)?;
+    }
+}
+
+pub fn run(input: &str) -> Result<(), anyhow::Error> {
+    println!("Got: {}", input);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::env;
-fn main() {
+fn main() -> Result<(), anyhow::Error> {
     let mut args = env::args().into_iter().skip(1).collect::<Vec<_>>();
-    if args.len() > 1 {
-        println!("Usage: clox [script]");
-        std::process::exit(64);
-    } else if args.len() == 1 {
-        let filename = args.pop().unwrap();
-        clox::run_file(filename);
-    } else {
-        clox::run_prompt();
+
+    match args.len() {
+        1 => {
+            let filename = args.pop().unwrap();
+            clox::run_file(filename.as_ref())
+        }
+        2.. => {
+            println!("Usage: clox [script]");
+            std::process::exit(64);
+        }
+        _ => clox::run_prompt(),
     }
 }
