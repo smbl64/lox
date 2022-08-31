6ca2c75df57940e2269e5fd2be918443365fb84b
Mohammad Banisaeid <smbl64@gmail.com>
2022-08-31 20:19:47 +0200

    Add a simple test

---
+++ tests/scanner.rs
@@ -0,0 +1,10 @@
+use rlox::prelude::Scanner;
+
+#[test]
+fn scanner_works() {
+    let input = "2 and 3";
+    let mut scanner = Scanner::new(input);
+    let tokens = scanner.scan_tokens();
+    //dbg!(&tokens);
+    assert_eq!(tokens.len(), 3);
+}
