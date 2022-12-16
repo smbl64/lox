db85671df22172e2650adb41453f34c29fdde027
Mohammad Banisaeid <smbl64@gmail.com>
2022-12-16 20:46:00 +0100

    Cleanup the entry point

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,16 +5,13 @@ use lox::Lox;
 fn main() -> Result<(), anyhow::Error> {
     let mut args = env::args().into_iter().skip(1).collect::<Vec<_>>();
 
-    let mut lox = Lox::new();
-    match args.len() {
-        1 => {
-            let filename = args.pop().unwrap();
-            lox.run_file(filename.as_ref())
-        }
-        _ => {
-            let bin_name = env!("CARGO_BIN_NAME");
-            println!("Usage: {} [script]", bin_name);
-            std::process::exit(64);
-        }
+    if args.len() == 1 {
+        let mut lox = Lox::new();
+        let filename = args.pop().unwrap();
+        lox.run_file(filename.as_ref())
+    } else {
+        let bin_name = env!("CARGO_BIN_NAME");
+        println!("Usage: {} <script>", bin_name);
+        std::process::exit(64);
     }
 }
